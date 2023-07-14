be4df7da818e91bfcd5759758c2d8767a3090121
lamine <lamine.idjeri@medavie.bluecross.ca>
2023-07-14 11:26:26 -0300

    MV-1: main methods that reads the contents of a text file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,22 @@
 /**
  * @author lamine
  */
-public class Main {
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "path/to/your/file.txt"; // Specify the path to your text file
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }
+    }
 }
